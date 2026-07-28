e601727b2d97750f97cb798b1beeef07a4a75634
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-07-28 16:35:16 +0900

    feat: Spring Boot 프로젝트 초기 설정 + Docker 개발 환경 구성

---
+++ src/main/java/com/_geul2geul/songeul/SongeulApplication.java
@@ -0,0 +1,13 @@
+package com._geul2geul.songeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongeulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_geul2geul/songeul/SongeulApplicationTests.java
@@ -0,0 +1,13 @@
+package com._geul2geul.songeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongeulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
