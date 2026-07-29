b71c88f57264d792f27c37a38a41eda2b84bd375
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-07-30 04:16:25 +0900

    feat: UserRepository에 findByPhoneNumber 추가

--- src/main/java/com/_geul2geul/songeul/user/repository/UserRepository.java
+++ src/main/java/com/_geul2geul/songeul/user/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com._geul2geul.songeul.user.repository;
 import com._geul2geul.songeul.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository <User, Long> {
     boolean existsByPhoneNumber(String phoneNumber);
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
