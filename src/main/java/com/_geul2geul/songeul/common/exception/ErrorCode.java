65eedd9165ec37cec064eb5dc4070ae0fe904645
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-08-01 02:56:46 +0900

    feat: 송금 결과 조회 API용 ErrorCode 추가 (E404)

--- src/main/java/com/_geul2geul/songeul/common/exception/ErrorCode.java
+++ src/main/java/com/_geul2geul/songeul/common/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     INVALID_TRANSFER_INFO(HttpStatus.BAD_REQUEST,"E400", "계좌번호와 금액을 모두 입력해야 송금할 수 있습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E401", "전화번호 또는 비밀번호가 올바르지 않습니다."),
     REMITTANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "송금 정보를 찾을 수 없습니다."),
-    ALREADY_TRANSFERRED(HttpStatus.CONFLICT, "E409", "이미 송금이 완료된 건입니다.");
+    ALREADY_TRANSFERRED(HttpStatus.CONFLICT, "E409", "이미 송금이 완료된 건입니다."),
+    TRANSFER_NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "송금 내역이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
