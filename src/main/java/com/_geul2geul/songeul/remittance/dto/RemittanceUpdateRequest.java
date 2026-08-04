b12269c5a0c2a72667d76bb717d8cb60407bca56
박재영 <pjyng115@naver.com>
2026-08-05 02:43:38 +0900

    fix: RemittanceUpdateRequest Swagger 예시값 명시
    amount 기본 예시가 0으로 나와서 Try it out에서 수정 없이 바로 실행하면
    @Positive 검증에 걸려 400이 발생하는 문제. 명세서 예시값으로 고정

--- src/main/java/com/_geul2geul/songeul/remittance/dto/RemittanceUpdateRequest.java
+++ src/main/java/com/_geul2geul/songeul/remittance/dto/RemittanceUpdateRequest.java
@@ -1,5 +1,6 @@
 package com._geul2geul.songeul.remittance.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,10 +13,16 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RemittanceUpdateRequest {
 
+    @Schema(example = "박영수")
     private String recipientName;
+
+    @Schema(example = "국민은행")
     private String bankName;
+
+    @Schema(example = "123-456-789012")
     private String accountNumber;
 
+    @Schema(example = "50000")
     @Positive(message = "송금액은 0보다 큰 숫자여야 합니다")
     private Long amount;
 
