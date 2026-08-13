7f101ebe12e69edc0954debf9194ef483eebd1c9
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-08-14 02:03:01 +0900

    fix: CORS 허용 메서드에 PATCH 추가

--- src/main/java/com/_geul2geul/songeul/common/config/SecurityConfig.java
+++ src/main/java/com/_geul2geul/songeul/common/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://songeul-tau.vercel.app", "http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
